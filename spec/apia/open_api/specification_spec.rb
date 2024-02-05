17ce234a75ac209a068656d061ff5d034bc95cea
Paul Sturgess <paul.sturgess@krystal.uk>
2024-02-05 11:24:21 +0000

    test: automatically validate generated schema (#58)
    closes: https://github.com/krystal/apia-openapi/issues/6

--- spec/apia/open_api/specification_spec.rb
+++ spec/apia/open_api/specification_spec.rb
@@ -2,18 +2,31 @@
 
 require "spec_helper"
 require "core_api/base"
+require "openapi3_parser"
 
 RSpec.describe Apia::OpenApi::Specification do
+  let(:fixture_path) { File.expand_path("spec/support/fixtures/openapi.json") }
+
   describe "#json" do
-    it "produces OpenAPI JSON" do
+    it "produces the expected OpenAPI JSON" do
       base_url = "http://127.0.0.1:9292/core/v1/"
       example_api = CoreAPI::Base
 
       spec = described_class.new(example_api, base_url, "Core")
 
-      expected_spec = File.read("spec/support/fixtures/openapi.json").strip
+      expected_spec = File.read(fixture_path).strip
 
       expect(spec.json).to eq(expected_spec)
+    end
+
+    # Obviously the fixture in this spec is hard-coded by us.
+    # But the test above forces us to keep the fixture up to date.
+    # And so each time we update the fixture, this test gives us
+    # confidence that the resulting JSON is valid according to OpenAPI.
+    it "produces a valid spec" do
+      spec = Openapi3Parser.load_file(fixture_path)
+
+      expect(spec.valid?).to be true
     end
   end
 end
